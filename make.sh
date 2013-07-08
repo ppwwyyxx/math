20dab7be1c7ffe29e5fed3c6c2912efecd514e59
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-07-08 21:43:09 +0800

    fix script, makefile work on cross-reference

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # File: make.sh
-# Date: Mon Jul 08 16:48:10 2013 +0800
+# Date: Mon Jul 08 21:34:09 2013 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 rm -rf output *.pdf
 for i in `find -maxdepth 1 -type d -not -path './.*'`; do
 	echo $i
-	make -C $i
+	make -C $i final
 	cp $i/output/*.pdf ./
 done
 mkdir -p output
